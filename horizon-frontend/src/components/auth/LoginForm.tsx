0ade3d93dcf0faab4b2325e38982001cb797db20
masonrs2 <masonsepulveda@gmail.com>
2025-05-05 02:03:25 -0500

    update register button url path in login page

--- horizon-frontend/src/components/auth/LoginForm.tsx
+++ horizon-frontend/src/components/auth/LoginForm.tsx
@@ -103,7 +103,7 @@ export function LoginForm() {
 
         <div className="text-center text-sm">
           Don't have an account?{' '}
-          <Link to="/register" className="text-primary hover:underline">
+          <Link to="/signup" className="text-primary hover:underline">
             Register
           </Link>
         </div>
